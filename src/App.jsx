2ab77357e733df60359da1b8b35c6290bc97b52a
appworkshopdb <app.workshop.db@gmail.com>
2026-07-31 17:03:16 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import ServicesPage from './components/ServicesPage';
 import TemplatesPage from './components/TemplatesPage';
 import InvoicesPage from './components/InvoicesPage';
 import InvoiceEditor from './components/InvoiceEditor';
+import DeliveryNotesPage from './components/DeliveryNotesPage';
+import DeliveryNoteEditor from './components/DeliveryNoteEditor';
+import CreditNotesPage from './components/CreditNotesPage';
+import CreditNoteEditor from './components/CreditNoteEditor';
+import RemindersPage from './components/RemindersPage';
+import ExpensesPage from './components/ExpensesPage';
+import OverviewPage from './components/OverviewPage';
 
 function AppInhalt() {
   const { session, ladeVorgang } = useAuth();
@@ -29,9 +36,16 @@ function AppInhalt() {
         <Route index element={<Navigate to="/rechnungen" replace />} />
         <Route path="/rechnungen" element={<InvoicesPage />} />
         <Route path="/rechnungen/:id" element={<InvoiceEditor />} />
+        <Route path="/lieferscheine" element={<DeliveryNotesPage />} />
+        <Route path="/lieferscheine/:id" element={<DeliveryNoteEditor />} />
+        <Route path="/gutschriften" element={<CreditNotesPage />} />
+        <Route path="/gutschriften/:id" element={<CreditNoteEditor />} />
+        <Route path="/mahnungen" element={<RemindersPage />} />
         <Route path="/vorlagen" element={<TemplatesPage />} />
         <Route path="/kunden" element={<CustomersPage />} />
         <Route path="/leistungen" element={<ServicesPage />} />
+        <Route path="/ausgaben" element={<ExpensesPage />} />
+        <Route path="/uebersicht" element={<OverviewPage />} />
         <Route path="*" element={<Navigate to="/rechnungen" replace />} />
       </Route>
     </Routes>
